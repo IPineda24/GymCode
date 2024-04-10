53088b9124c36e4be847297a17bf8310a80c3b10
irvin pineda <62679993+IPineda24@users.noreply.github.com>
2024-04-10 08:11:49 -0600

    Update main2.js

--- main2.js
+++ main2.js
@@ -1,6 +1,10 @@
 const countDays2 = document.getElementById('countDays2');
+const now = document.getElementById('now');
 const startDate2 = new Date('2024-04-10 00:00:00');
 const today2 = new Date();
 const diff2 = today2 - startDate2;
 const days2 = Math.floor(diff2 / (1000 * 60 * 60 * 24));
 countDays2.textContent = "Racha: " + days2 + " dias 🔥💪";
+
+const options = { year: 'numeric', month: 'long', day: 'numeric' };
+now.textContent = new Date().toLocaleDateString('es-ES', options);
